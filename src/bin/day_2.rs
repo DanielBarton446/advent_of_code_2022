cf565587a32d24819cb4fa0c556db8b79a676c46
Daniel.Barton446 <daniel.barton446@gmail.com>
2022-12-02 21:48:53 -0600

    adds readability

--- src/bin/day_2.rs
+++ src/bin/day_2.rs
@@ -1,9 +1,14 @@
-#![allow(dead_code)]
-const LOSE: i64 = 0;
-const TIE: i64 = 3;
-const WIN: i64 = 6;
+const ROCK:     i64 = 1;
+const PAPER:    i64 = 2;
+const SCISSORS: i64 = 3;
+const LOSE:     i64 = 0;
+const TIE:      i64 = 3;
+const WIN:      i64 = 6;
 
 fn translate_line(line: &[u8]) -> (i64, i64) {
+    // translate A -> 1, X -> 1
+    // translate B -> 2, Y -> 2
+    // translate C -> 3, Z -> 3
     ((line[0]-b'A'+1) as i64, (line[2]-b'X'+1) as i64)
 }
 
@@ -18,10 +23,12 @@ fn solve_p1(input: &str){
     let rounds = parse_input(input);
     let mut total: i64 = 0;
     for round in rounds{
-        total += match round.1 - round.0 {
-            0 => round.1 + TIE,
-            -1 | 2 => round.1 + LOSE,
-            -2 | 1 => round.1 + WIN,
+        let them = round.0;
+        let me = round.1;
+        total += match me - them {
+            -1 | 2 => LOSE + me,
+             0     => TIE  + me,
+            -2 | 1 => WIN  + me,
             _ => unreachable!("not a number")
         };
     }
@@ -31,12 +38,14 @@ fn solve_p1(input: &str){
 fn solve_p2(input: &str){
     let rounds = parse_input(input);
     let mut total: i64 = 0;
-    let vals = [1,2,3];
+    let state_decoding = [ROCK,PAPER,SCISSORS];
     for round in rounds{
-        total += match round.1 {
-            1 => vals[(round.0 as usize + 1) % 3] + LOSE,
-            2 => round.0 + TIE, // good
-            3 => vals[(round.0 as usize) % 3] + WIN,
+        let them_zero_indexed = (round.0 - 1) as usize;
+        let my_result = round.1;
+        total += match my_result {
+            1 => LOSE + state_decoding[(them_zero_indexed + 2) % 3],
+            2 => TIE  + state_decoding[(them_zero_indexed)     % 3], 
+            3 => WIN  + state_decoding[(them_zero_indexed + 1) % 3],
             _ => unreachable!("not a number")
         }
     }
